58ba6390d290ad55b3f138bdd360d2c06ba3f10f
amp <am.perescu@gmail.com>
2020-09-10 17:01:12 +0300

    Initial commit

---
+++ TodoApp/TodoListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Antonela Madalina on 10/09/2020.
+//  Copyright © 2020 AM Perescu. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let todoArray = ["Design iOS app", "Implement iOS app", "Launch iOS app"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = todoArray[indexPath.row]
+        return cell
+    }
+
+}
+
